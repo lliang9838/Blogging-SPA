b786d8673e2704c128f2375e385a87b80d43bc56
Leslie Liang <lliang9838@gmail.com>
2019-06-19 23:21:05 -0700

    deleted inmemory api, will debug using the way that cho suggested

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule }    from '@angular/common/http';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
 import { ListComponent } from './list/list.component';
 
 @NgModule({
@@ -15,14 +12,7 @@ import { ListComponent } from './list/list.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
